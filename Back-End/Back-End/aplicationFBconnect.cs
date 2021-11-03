2c14f4b97083e50ab4f73065e6a4dcd2ec9902c7
fantasmenginner <ingenieroromario@gmail.com>
2021-11-03 12:23:57 -0500

    proyecto terminado

---
+++ Back-End/Back-End/aplicationFBconnect.cs
@@ -0,0 +1,20 @@
+﻿using Back_End.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_End
+{
+    public class aplicationFBconnect : DbContext
+    {
+        public DbSet<Comentario> comentario { get; set; }
+
+        public aplicationFBconnect(DbContextOptions<aplicationFBconnect> options) : base(options)
+        {
+
+        }
+
+    }
+}
